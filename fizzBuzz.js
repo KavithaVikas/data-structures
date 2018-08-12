62b3d89ef661bc2216151e66ac5603d8037e87c2
xx18 <xx18@global.tesco.org>
2018-08-12 23:08:33 +0530

    fizzbuzz algorithm

---
+++ fizzBuzz.js
@@ -0,0 +1,26 @@
+var fizzBuzz = (num) => {
+
+    for(var i=1;i<num;i++){
+
+        if (i % 15 == 0) {
+            console.log(`${i} - fizzBuzz`);
+           
+        }
+        else if (i % 3 == 0) {
+            console.log(`${i} - fizz`);
+            
+        }
+        else if (i % 5 == 0) {
+            
+            console.log(num%5);
+            console.log(`${i} - buzz`);
+            
+        }
+        else{
+            console.log()
+        }
+        
+    }
+}
+
+fizzBuzz(16);
