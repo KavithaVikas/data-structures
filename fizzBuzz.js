5772c120ead3f3475af1789b259304d712a5bfbd
xx18 <xx18@global.tesco.org>
2018-08-12 23:25:45 +0530

    refactored

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -5,6 +5,7 @@ var fizzBuzz = (num) => {
         if (i % 15 === 0) console.log(`${i} - fizzBuzz`);
         else if (i % 3 === 0) console.log(`${i} - fizz`);
         else if (i % 5 === 0)  console.log(`${i} - buzz`);
+        else console.log(i)
     }
 }
 
